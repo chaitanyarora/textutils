37bf6917060e2bdf0694b87af969e8423627babc
chaitanyarora <arorachaitany15@gmail.com>
2023-03-28 14:44:40 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, { useState } from 'react'
+import Alert from './components/Alert';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const [mode, setMode] = useState("light")
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+
+  const toggleMode = ()=>{
+    if(mode === "light"){
+      setMode("dark")
+      document.body.style.backgroundColor = "black"
+      showAlert("Hero hogya dark", "success")
+    }
+    else{
+      setMode("light")
+      document.body.style.backgroundColor = "white"
+      showAlert("Hero hogya light", "success")
+      
+    }
+  }
+  return ( 
+    <>
+    <BrowserRouter>
+      <Navbar
+        title="TextUtils"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <Alert
+        alert={alert}
+      />
+      <Routes>
+        <Route exact path="/" element={
+          <TextForm
+          heading="Text Area"
+          mode={mode}
+          />
+        } />
+        <Route exact path="/about" element={<About
+          mode={mode}
+        />
+        } />
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        // console.log("Uppercase was clicked");
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+
+    const handleLoClick = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+
+    const handleClearClick = ()=>{
+        let newText = " ";
+        setText(newText)
+    }
+
+    const handleOnChange = (event)=>{
+        setText(event.target.value)
+    }
+
+    const [text, setText] = useState("");
+
+    return (
+        <>
+        <div className="container mb-3" style={{color: props.mode==="light"?"black":"white"}}>
+            <h1>{props.heading}</h1>
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==="dark"?"black":"white", color: props.mode==="light"?"black":"white"}} id="myBox" rows="8"></textarea>
+            <button className="btn btn-primary m-1" onClick={handleUpClick}>Uppercase</button>
+            <button className="btn btn-primary" onClick={handleLoClick}>Lowercase</button>
+            <button className="btn btn-primary m-1" onClick={handleClearClick}>Clear</button>
+        </div>
+        <div className="container my-3" style={{color: props.mode==="light"?"black":"white"}} >
+            <h1>Your text Summary</h1>
+            <p>Your total characters are {text.length} and words are {text.split(" ").length}</p>
+        </div>
+        </>
+    )
+}
